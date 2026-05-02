7e9691cc2c2c22628c3047554587d01f5d531a31
Nathan Oketch <nobuyanzi07@gmail.com>
2026-05-02 21:44:59 +0300

    feat(studentplanner.js): Creation of function section

--- studentplanner.js
+++ studentplanner.js
@@ -5,5 +5,21 @@ const taskList = document.getElementById("taskList");
 // Add task
 addbtn.addEventListener("click", addtask);
 
+function addtask() {
+    const task = taskInput.value.trim();
+    if (task) {
+        const li = document.createElement("li");
+        li.textContent = task;
+    }
+
+    // Create List Item
+    const li = document.createElement("li");
+    // Task Text
+    const span = document.createElement("span");
+    span.textContent = taskText;
+    span.classList.add("task-text");
+    li.appendChild(span);
+
+
 
 
